551e75c8900b6a235792f785d08ccbcd657fd89f
ksooo091 <hwanrim00@gmail.com>
2023-08-30 17:28:20 +0900

    [style] fmt

--- dev/locals.tf
+++ dev/locals.tf
@@ -1,20 +1,15 @@
 locals {
-
   use_az = [
     "${var.use_region}a",
     "${var.use_region}b"
   ]
 }
 
-
-
-
 locals {
   resource_tags = {
     ProjectEnv = var.project_env
   }
   suffix_name = var.project_env
-
 }
 
 locals {
