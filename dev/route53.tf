c1daa217c6f43eb316d2a0fc10d3a4801e40e855
ksooo091 <hwanrim00@gmail.com>
2023-08-30 21:45:19 +0900

    [style] fmt

--- dev/route53.tf
+++ dev/route53.tf
@@ -1,8 +1,8 @@
- resource "aws_route53_zone" "primary" {
-   name = "https://${var.ourdomain}"
- }
+resource "aws_route53_zone" "primary" {
+  name = "https://${var.ourdomain}"
+}
 
- resource "aws_acm_certificate" "cert" {
+resource "aws_acm_certificate" "cert" {
   domain_name       = "*.${var.ourdomain}"
   validation_method = "DNS"
 
