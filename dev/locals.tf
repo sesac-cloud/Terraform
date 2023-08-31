63ee7cde7b88a8dc954ed4ac8912035863a6fd4b
ksooo091 <hwanrim00@gmail.com>
2023-08-31 20:28:08 +0900

    [fix] eks 정책

--- dev/locals.tf
+++ dev/locals.tf
@@ -22,5 +22,5 @@ locals {
 
 locals {
   eks_cluster_policy    = ["arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"]
-  eks_node_group_policy = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly", "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy","arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"]
+  eks_node_group_policy = ["arn:aws:iam:aws:policy/AmazonSSMManagedInstanceCore", "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly", "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy", "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"]
 }
