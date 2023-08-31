0995c301371ca53879350ea559acbb42983d4b1e
ksooo091 <hwanrim00@gmail.com>
2023-08-31 09:19:21 +0900

    [fix]

--- dev/eksAddon.tf
+++ dev/eksAddon.tf
@@ -1,30 +1,30 @@
-module "eks_blueprints_addons" {
-  source = "aws-ia/eks-blueprints-addons/aws"
+# module "eks_blueprints_addons" {
+#   source = "aws-ia/eks-blueprints-addons/aws"
 
-  cluster_name      = aws_eks_cluster.eks_cluster.name
-  cluster_endpoint  = aws_eks_cluster.eks_cluster.endpoint
-  cluster_version   = aws_eks_cluster.eks_cluster.version
-  oidc_provider_arn = aws_eks_identity_provider_config.oidc_config.arn
+#   cluster_name      = aws_eks_cluster.eks_cluster.name
+#   cluster_endpoint  = aws_eks_cluster.eks_cluster.endpoint
+#   cluster_version   = aws_eks_cluster.eks_cluster.version
+#   oidc_provider_arn = aws_eks_identity_provider_config.oidc_config.arn
 
-  eks_addons = {
-    aws-ebs-csi-driver = {
-      most_recent = true
-    }
-    coredns = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-  }
+#   eks_addons = {
+#     aws-ebs-csi-driver = {
+#       most_recent = true
+#     }
+#     coredns = {
+#       most_recent = true
+#     }
+#     vpc-cni = {
+#       most_recent = true
+#     }
+#     kube-proxy = {
+#       most_recent = true
+#     }
+#   }
 
-  enable_aws_load_balancer_controller    = true
-  enable_cluster_proportional_autoscaler = true
-  enable_karpenter                       = true
-  enable_metrics_server                  = true
-  enable_external_dns                    = true
+#   enable_aws_load_balancer_controller    = true
+#   enable_cluster_proportional_autoscaler = true
+#   enable_karpenter                       = true
+#   enable_metrics_server                  = true
+#   enable_external_dns                    = true
 
-}
+# }
