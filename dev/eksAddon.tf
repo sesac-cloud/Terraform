d8dc8d9046fb08fd64ed7cc1a0d92742ddcf09e2
ksooo091 <hwanrim00@gmail.com>
2023-08-31 15:17:59 +0900

    애드온지움

--- dev/eksAddon.tf
+++ dev/eksAddon.tf
@@ -1,20 +1,6 @@
-module "eks_blueprints_addons" {
-  source = "aws-ia/eks-blueprints-addons/aws"
 
-  cluster_name      = aws_eks_cluster.eks_cluster.name
-  cluster_endpoint  = aws_eks_cluster.eks_cluster.endpoint
-  cluster_version   = aws_eks_cluster.eks_cluster.version
-  oidc_provider_arn = aws_iam_openid_connect_provider.iamoidc.arn
 
-  eks_addons = {
-    aws-ebs-csi-driver = {
-      most_recent = true
-    }
-  }
-
-  enable_aws_load_balancer_controller    = true
-  enable_karpenter                       = true
-  enable_metrics_server                  = true
-  enable_external_dns                    = true
-
-}
+# resource "aws_eks_addon" "example" {
+#   cluster_name = aws_eks_cluster.example.name
+#   addon_name   = "aws-ebs-csi-driver"
+# }
