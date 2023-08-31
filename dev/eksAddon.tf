eb667162b9bd2c3e8afcfd2f1775ca1bdf066581
ksooo091 <hwanrim00@gmail.com>
2023-08-31 15:24:25 +0900

    [feat] ebs csi 설치

--- dev/eksAddon.tf
+++ dev/eksAddon.tf
@@ -1,6 +1,4 @@
-
-
-# resource "aws_eks_addon" "example" {
-#   cluster_name = aws_eks_cluster.example.name
-#   addon_name   = "aws-ebs-csi-driver"
-# }
+resource "aws_eks_addon" "example" {
+  cluster_name = aws_eks_cluster.example.name
+  addon_name   = "aws-ebs-csi-driver"
+}
