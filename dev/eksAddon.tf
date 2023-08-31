df55182a8d6c131cfffb3c9adff5245d568e5aae
ksooo091 <hwanrim00@gmail.com>
2023-08-31 16:28:45 +0900

    eksaddon

--- dev/eksAddon.tf
+++ dev/eksAddon.tf
@@ -1,4 +1,6 @@
-resource "aws_eks_addon" "example" {
-  cluster_name = aws_eks_cluster.example.name
+
+resource "aws_eks_addon" "ebs-csi-drvier" {
+  cluster_name = aws_eks_cluster.eks_cluster.name
   addon_name   = "aws-ebs-csi-driver"
 }
+
